290060df2f0a31d64e4b3eb25e52006f02344fae
seanpenn2011 <seanpenn2011@hotmail.com>
2022-01-04 13:18:11 +0800

    2 files modified, 3 added

---
+++ src/app/employees/employee.component.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormArray, FormBuilder, FormControl } from '@angular/forms';
+
+@Component({
+  selector: 'employees',
+  templateUrl: './employee.component.html',
+  styleUrls: ['./employee.component.css'],
+})
+export class EmployeeComponent implements OnInit {
+  ngOnInit() {}
+}
